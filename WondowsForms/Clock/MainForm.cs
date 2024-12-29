441c1a45e5872d5138d4a0ed16b985a02343e437
Alexander Zarubin <whitesashka@gmail.com>
2024-12-29 17:42:34 +0500

    TODO : HideControls button

---
+++ WondowsForms/Clock/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			//Обработчик события - это самая обычная функция, которая не явно вызывается при возникновоний определеного события
+			// У элемента интерфейса может быть множество событий, и одно из них будет событием по умолчанию
+			//labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt",System.Globalization.CultureInfo.InvariantCulture);
+			labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+		if(checkBoxShowDate.Checked)
+			labelTime.Text+=$"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+			if (checkBoxShowWeekDay.Checked)
+				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+		}
+
+
+	}
+}
